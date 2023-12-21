c98881becf5cacd649082d3db172ced580e712ce
Harm Meiejr <harm.meijer@commercetools.com>
2023-12-21 10:22:07 +0100

    get and update custom object

--- src/components/channels/channels.tsx
+++ src/components/channels/channels.tsx
@@ -1,140 +1,52 @@
-import { useIntl } from 'react-intl';
 import {
-  Link as RouterLink,
-  Switch,
-  useHistory,
-  useRouteMatch,
-} from 'react-router-dom';
-import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
-import { NO_VALUE_FALLBACK } from '@commercetools-frontend/constants';
-import {
-  usePaginationState,
-  useDataTableSortingState,
-} from '@commercetools-uikit/hooks';
-import { BackIcon } from '@commercetools-uikit/icons';
-import Constraints from '@commercetools-uikit/constraints';
-import FlatButton from '@commercetools-uikit/flat-button';
-import LoadingSpinner from '@commercetools-uikit/loading-spinner';
-import DataTable from '@commercetools-uikit/data-table';
-import { ContentNotification } from '@commercetools-uikit/notifications';
-import { Pagination } from '@commercetools-uikit/pagination';
-import Spacings from '@commercetools-uikit/spacings';
-import Text from '@commercetools-uikit/text';
-import { SuspendedRoute } from '@commercetools-frontend/application-shell';
-import {
-  formatLocalizedString,
-  transformLocalizedFieldToLocalizedString,
-} from '@commercetools-frontend/l10n';
-import type { TFetchChannelsQuery } from '../../types/generated/ctp';
-import { useChannelsFetcher } from '../../hooks/use-channels-connector';
-import { getErrorMessage } from '../../helpers';
-import messages from './messages';
-import ChannelDetails from '../channel-details';
+  useMcMutation,
+  useMcQuery,
+} from '@commercetools-frontend/application-shell-connectors';
+import UpdateCustomObjectMutation from '../../hooks/use-channels-connector/update-custom-object.ctp.graphql';
+import FetchCustomObjectQuery from '../../hooks/use-channels-connector/fetch-custom-object.ctp.graphql';
+import { GRAPHQL_TARGETS } from '@commercetools-frontend/constants';
+import { useCallback } from 'react';
 
-const columns = [
-  { key: 'name', label: 'Channel name' },
-  { key: 'key', label: 'Channel key', isSortable: true },
-  { key: 'roles', label: 'Roles' },
-];
-
-type TChannelsProps = {
-  linkToWelcome: string;
-};
-
-const Channels = (props: TChannelsProps) => {
-  const intl = useIntl();
-  const match = useRouteMatch();
-  const { push } = useHistory();
-  const { page, perPage } = usePaginationState();
-  const tableSorting = useDataTableSortingState({ key: 'key', order: 'asc' });
-  const { dataLocale, projectLanguages } = useApplicationContext((context) => ({
-    dataLocale: context.dataLocale,
-    projectLanguages: context.project?.languages,
-  }));
-  const { channelsPaginatedResult, error, loading } = useChannelsFetcher({
-    page,
-    perPage,
-    tableSorting,
+const Channels = () => {
+  const { data, loading, refetch } = useMcQuery(FetchCustomObjectQuery, {
+    variables: {
+      key: 'certification-comments-af6c6cf0-f19b-4101-b761-b6f139a2b0e0',
+      container: 'certification-comments',
+    },
+    context: {
+      target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM,
+    },
   });
-
-  if (error) {
-    return (
-      <ContentNotification type="error">
-        <Text.Body>{getErrorMessage(error)}</Text.Body>
-      </ContentNotification>
-    );
-  }
-
+  const [updateCustomObject, { loading: updating }] = useMcMutation(
+    UpdateCustomObjectMutation
+  );
+  //@ts-ignore
+  const version = data?.customObject?.version;
+  const update = useCallback(() => {
+    updateCustomObject({
+      context: {
+        target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM,
+      },
+      variables: {
+        version,
+        key: 'certification-comments-af6c6cf0-f19b-4101-b761-b6f139a2b0e0',
+        container: 'certification-comments',
+        value: JSON.stringify({ field: `Random: ${Math.random()}` }),
+      },
+    }).then(() => {
+      refetch();
+    });
+  }, [refetch, updateCustomObject, version]);
+  console.log('loading is not set correctly again', loading);
   return (
-    <Spacings.Stack scale="xl">
-      <Spacings.Stack scale="xs">
-        <FlatButton
-          as={RouterLink}
-          to={props.linkToWelcome}
-          label={intl.formatMessage(messages.backToWelcome)}
-          icon={<BackIcon />}
-        />
-        <Text.Headline as="h2" intlMessage={messages.title} />
-      </Spacings.Stack>
-
-      <Constraints.Horizontal max={13}>
-        <ContentNotification type="info">
-          <Text.Body intlMessage={messages.demoHint} />
-        </ContentNotification>
-      </Constraints.Horizontal>
-
-      {loading && <LoadingSpinner />}
-
-      {channelsPaginatedResult ? (
-        <Spacings.Stack scale="l">
-          <DataTable<NonNullable<TFetchChannelsQuery['channels']['results']>[0]>
-            isCondensed
-            columns={columns}
-            rows={channelsPaginatedResult.results}
-            itemRenderer={(item, column) => {
-              switch (column.key) {
-                case 'key':
-                  return item.key;
-                case 'roles':
-                  return item.roles.join(', ');
-                case 'name':
-                  return formatLocalizedString(
-                    {
-                      name: transformLocalizedFieldToLocalizedString(
-                        item.nameAllLocales ?? []
-                      ),
-                    },
-                    {
-                      key: 'name',
-                      locale: dataLocale,
-                      fallbackOrder: projectLanguages,
-                      fallback: NO_VALUE_FALLBACK,
-                    }
-                  );
-                default:
-                  return null;
-              }
-            }}
-            sortedBy={tableSorting.value.key}
-            sortDirection={tableSorting.value.order}
-            onSortChange={tableSorting.onChange}
-            onRowClick={(row) => push(`${match.url}/${row.id}`)}
-          />
-          <Pagination
-            page={page.value}
-            onPageChange={page.onChange}
-            perPage={perPage.value}
-            onPerPageChange={perPage.onChange}
-            totalItems={channelsPaginatedResult.total}
-          />
-          <Switch>
-            <SuspendedRoute path={`${match.url}/:id`}>
-              <ChannelDetails onClose={() => push(`${match.url}`)} />
-            </SuspendedRoute>
-          </Switch>
-        </Spacings.Stack>
-      ) : null}
-    </Spacings.Stack>
+    <div>
+      {!loading && <pre>{JSON.stringify(data, undefined, 2)}</pre>}
+      {!loading && (
+        <button onClick={update} disabled={updating}>
+          update
+        </button>
+      )}
+    </div>
   );
 };
 Channels.displayName = 'Channels';
